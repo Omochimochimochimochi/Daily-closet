00aab7b86eb0706a7516d37d3f05da749d5f91cf
Omochimochimochimochi <crossroad.nnnnn@gmail.com>
2026-03-22 09:54:24 +0900

    urls.py を更新

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('search/', views.item_search, name='item_search'),
     
 
-    # --- パスワード再設定（Django標準機能を使用） ---
+    # --- パスワード再設定---
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
