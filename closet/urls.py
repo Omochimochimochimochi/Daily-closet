6e3fb248710642b2f2ccbb8fccdc2f5958b7c9fd
Omochimochimochimochi <crossroad.nnnnn@gmail.com>
2026-03-25 15:43:58 +0900

    urls.py修正

--- closet/urls.py
+++ closet/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views  
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    path('', include('closet.urls')), 
+
+    path('', views.top, name='top'),
+    
+    path('admin-login/', views.admin_login, name='admin_login'),
+    path('admin-menu/', views.admin_menu, name='admin_menu'),
+    path('admin-inventory/', views.inventory_manage, name='inventory_manage'),
+    path('admin-inventory/add/', views.item_register, name='item_register'),
 ]
