dc0ad57b4a264ae9f3f46a5facec5fa30ae5c45e
Omochimochimochimochi <crossroad.nnnnn@gmail.com>
2026-03-29 22:39:32 +0900

    動きを修正

--- closet/urls.py
+++ closet/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views  
-urlpatterns = [
+# closet/urls.py
 
+urlpatterns = [
     path('', views.top, name='top'),
     
-    path('admin-login/', views.admin_login, name='admin_login'),
+    path('admin-login/', views.admin_login, name='login'), 
+    
+    path('signup/', views.admin_menu, name='signup'), 
+    
     path('admin-menu/', views.admin_menu, name='admin_menu'),
     path('admin-inventory/', views.inventory_manage, name='inventory_manage'),
     path('admin-inventory/add/', views.item_register, name='item_register'),
     path('admin-items/', views.admin_item_list, name='admin_item_list'),
 ]
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,26 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views # Django標準のビューをインポート
-from closet import views 
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.top, name='top'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
-    path('signup/complete/', views.signup_complete, name='signup_complete'),
-    path('mypage/', views.mypage, name='mypage'),
-    path('email/change', views.email_change, name='email_change'),
-    path('password/change', views.password_change, name='password_change'),
-    path('search/', views.item_search, name='item_search'),
-    
-
-    # --- パスワード再設定---
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-
+    path('', include('closet.urls')), 
 ]
