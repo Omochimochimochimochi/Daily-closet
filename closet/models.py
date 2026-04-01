41927c0a3bc720669f52d792bc3d83559f6701b5
Omochimochimochimochi <crossroad.nnnnn@gmail.com>
2026-04-01 22:32:02 +0900

    アイテム追加画面上にタグ一覧を追加、保存押下し実際のアイテム一覧へ追加できたことを確認。

--- closet/models.py
+++ closet/models.py
@@ -1,38 +1,30 @@
-from django.db import models  
+# views.py の中身を以下のように修正
+def item_register(request):
+    if request.method == 'POST':
 
-class Item(models.Model):
-    # 基本情報
-    brand_name = models.CharField("ブランド名", max_length=100, blank=True)
-    item_name = models.CharField("アイテム名", max_length=100)
-    price = models.IntegerField("金額", default=0)
-    color = models.CharField("カラー", max_length=50, blank=True)
-    image = models.ImageField("アイテム画像", upload_to='items/', blank=True, null=True)
+        name_val = request.POST.get('name')   
+        brand_val = request.POST.get('brand')
+        price_val = request.POST.get('price')
+        color_val = request.POST.get('color')
+        image_file = request.FILES.get('image')
 
-  
-    STYLE_CHOICES = [
-        ('simple', '#シンプル'),
-        ('kireime', '#きれいめ'),
-        ('aline', '#Aライン'),
-        ('iline', '#Iライン'),
-        ('sale', '#セール'),
-    ]
-    style = models.CharField("スタイル", max_length=20, choices=STYLE_CHOICES, blank=True)
+        styles = request.POST.getlist('style')
+        kokkakus = request.POST.getlist('kokkaku')
+        colors = request.POST.getlist('personal_color')
+        free_tags_val = request.POST.get('free_tags', "")
 
-    KOKKAKU_CHOICES = [
-        ('straight', '#ストレート'),
-        ('wave', '#ウェーブ'),
-        ('natural', '#ナチュラル'),
-    ]
-    kokkaku = models.CharField("骨格タイプ", max_length=20, choices=KOKKAKU_CHOICES, blank=True)
+        item = Item.objects.create(
+            item_name=name_val,        
+            brand_name=brand_val,      
+            price=price_val,           
+            color=color_val,          
+            image=image_file,        
+            style=",".join(styles),
+            kokkaku=",".join(kokkakus),
+            personal_color=",".join(colors),
+            free_tags=free_tags_val
+        )
+        
+        return redirect('inventory_manage')
 
-    COLOR_TYPE_CHOICES = [
-        ('yebe', '#イエベ'),
-        ('burube', '#ブルベ'),
-        ('neutral', '#ニュートラル'),
-    ]
-    personal_color = models.CharField("パーソナルカラー", max_length=20, choices=COLOR_TYPE_CHOICES, blank=True)
-
-    free_tags = models.TextField("自由記入タグ", blank=True)
-
-    def __str__(self):
-        return self.item_name
+    return render(request, 'item_register.html')
